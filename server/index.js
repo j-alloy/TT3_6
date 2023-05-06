82a3b72d0e13785baa8a781cc17f9de8b0b8fec3
HonWek <honwek_lim@live.com.my>
2023-05-06 15:08:14 +0800

    read all tables on database

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from "express"
 import cors from "cors"
 import "./loadEnvironment.js"
+import currency from "./routes/currency.js"
+import department from "./routes/department.js"
 import employee from "./routes/employee.js"
+import employeeprojects from "./routes/employeeprojects.js"
+import projectexpenseclaims from "./routes/projectexpenseclaims.js"
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -10,6 +14,10 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/employee", employee)
+app.use("/employeeprojects", employeeprojects)
+app.use("/projectexpenseclaims", projectexpenseclaims)
+app.use("/currency", currency)
+app.use("/department", department)
 
 app.listen(5000, () => {
   console.log(`Server Running on Port ${PORT}`);
