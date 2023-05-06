4ee3f1aa72be78257d81b425af058ae92f0b8ddb
gimleng <e0426020@u.nus.edu>
2023-05-06 11:56:55 +0800

    add login endpoint

--- server/index.js
+++ server/index.js
@@ -10,10 +10,26 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// Use body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // mongoose.connect(process.env.CONNECTION_URL).then(() => {
 //   console.log(`Server Running on Port ${PORT}`);
 // });
 
 app.listen(5000, () => {
   console.log("Port Running");
+});
+
+// Login endpoint
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ msg: 'Please enter both username and password' });
+  }
+
+  res.json({ msg: 'Logged in successfully', user: { id: 2, username: username } });
+  
 });
