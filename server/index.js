cf240201b457b916ce50b99e55d48d71b4b384e3
Jun Yi <90788342+j-alloy@users.noreply.github.com>
2023-05-06 11:15:55 +0800

    update index.js to run server

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+// import mongoose from "mongoose";
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// mongoose.connect(process.env.CONNECTION_URL).then(() => {
+//   console.log(`Server Running on Port ${PORT}`);
+// });
+
+app.listen(5000, () => {
+  console.log("Port Running");
+});
