70f0e814bd4c0aead53a3d5103e33b8bc259c00c
angelaying <87664873+angelayinggg@users.noreply.github.com>
2023-05-06 17:00:31 +0800

    changes to crud

--- server/routes/projectexpenseclaims.js
+++ server/routes/projectexpenseclaims.js
@@ -1,10 +1,12 @@
 import express from "express"
-import { getClaims, createClaims } from "../controllers/claims.js"
+import { getClaims, createClaims, updateClaims, deleteClaims } from "../controllers/claims.js"
 
 const router = express.Router()
 
 
 router.get("/", getClaims)
 router.post("/", createClaims)
+router.put("/", updateClaims)
+router.delete("/", deleteClaims)
 
 export default router
